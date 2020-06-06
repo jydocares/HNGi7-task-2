41a02675d36b0085c9cada4d544c5f2b0df7287b
jydocares <61963773+jydocares@users.noreply.github.com>
2020-06-06 13:00:56 +0100

    Adeboyejide-HNG-05452 script

---
+++ scripts/Adeboye-HNG-05452.py
@@ -0,0 +1,5 @@
+name = "Adeboye Jide"
+HNG_ID = "HNG-05452"
+language = "Python"
+email_address = "jydocares@yahoo.com"
+print("Hello world, this is {} with HNGi7 ID {} and email {} using {} for stage 2 task".format(name, HNG_ID, email_address, language))
